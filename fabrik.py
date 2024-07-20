080873b9ed8926372731351cda76fb23aeed1c56
johnjeong07 <23105@sshs.hs.kr>
2024-07-20 11:35:34 +0900

    Research Ended

--- fabrik.py
+++ fabrik.py
@@ -2,20 +2,9 @@ import time
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-from functools import wraps
 
 dist=lambda a,b:((((a[0]-b[0])**2)+((a[1]-b[1])**2))**0.5)
 unit_vector=lambda v:v/np.linalg.norm(v)
-
-def timer(func):
-    @wraps(func)
-    def wrapper(*args,**kwargs):
-        start=time.time()
-        result=func(*args,**kwargs)
-        end=time.time()
-        print(f"{func.__name__} Execution time:{end-start}")
-        return result
-    return wrapper
 
 class Structure:
     def __init__(self,structure:np.ndarray,length:np.ndarray)->None:
@@ -35,7 +24,7 @@ class Structure:
         plt.show()
 
 class Fabrik:
-    def __init__(self,model:Structure)->None:
+    def fit_model(self,model:Structure)->None:
         self.base=model.structure[0].copy()
         self.structure=model.structure.copy()
         self.length=model.length.copy()
@@ -46,52 +35,90 @@ class Fabrik:
             return False
         return True
 
-    def forward(self):
+    def __forward(self):
         self.structure[-1]=self.target
         for i in range(1,self.structure.shape[0]):
             self.structure[-i-1]=self.structure[-i]+unit_vector(self.structure[-i-1]-self.structure[-i])*self.length[-i]
 
-    def backward(self):
+    def __backward(self):
         self.structure[0]=self.base
         for i  in range(0,self.structure.shape[0]-1):
             self.structure[i+1]=self.structure[i]+unit_vector(self.structure[i+1]-self.structure[i])*self.length[i]
 
-    def solve(self,target:np.ndarray,tolerance:float)->np.ndarray:
-        if self.isreachable(target):
-            self.target=target.copy()
-            cnt=0
-            while (diff:=dist(self.structure[-1],self.target))>tolerance:
-                self.forward()
-                self.backward()
-                cnt+=1
-        else:
-            return self.structure
-
-    def forward1(self):
-        self.structure[-1]=(self.a*unit_vector(self.structure[1]-self.structure[0]))*np.dot((self.structure[1]-self.structure[0]),self.target-self.structure[-1])/np.sum(self.length)+(self.b*unit_vector(self.target-self.structure[0]))*(np.dot(self.target-self.structure[0],self.target-self.structure[-1])/np.sum(self.length))+self.structure[-1]
+    def __forward1(self,a:float,b:float):
+        self.structure[-1]=self.__fake_target(a,b)
+        # self.structure[-1]=(a*unit_vector(self.structure[1]-self.structure[0]))*np.dot((self.structure[1]-self.structure[0]),self.target-self.structure[-1])/np.sum(self.length)+(b*unit_vector(self.target-self.structure[0]))*(np.dot(self.target-self.structure[0],self.target-self.structure[-1])/np.sum(self.length))+self.target
         for i in range(1,self.structure.shape[0]):
             self.structure[-i-1]=self.structure[-i]+unit_vector(self.structure[-i-1]-self.structure[-i])*self.length[-i]
 
-    def solve1(self,target:np.ndarray,tolerance:float,a:float,b:float)->np.ndarray:
-        self.a=a
-        self.b=b
+    def __fake_target(self,a:float,b:float):
+        fake_target=self.target.copy()
+        total_length=np.sum(self.length)
+        fake_target+=a*unit_vector(self.structure[1]-self.structure[0])*np.dot(self.structure[1]-self.structure[0],fake_target-self.structure[-1])/total_length
+        fake_target+=b*unit_vector(fake_target-self.structure[0])*np.dot(fake_target-self.structure[0],fake_target-self.structure[-1])/total_length
+
+        return fake_target
+
+    def solve(self,target:np.ndarray,tolerance:float=0.05,track:bool=False,name:str='wow')->(float,int,np.ndarray):
+        if track:
+            track=np.ndarray()
+            np.concatenate((track,self.structure),axis=0)
         if self.isreachable(target):
             self.target=target.copy()
             cnt=0
+            st=time.time()
             while (diff:=dist(self.structure[-1],self.target))>tolerance:
-                self.forward1()
-                self.backward()
+                self.__forward()
+                self.__backward()
                 cnt+=1
+                if track:
+                    np.concatenate((track,self.structure),axis=0)
+
+                if cnt>200:
+                    print("This Algorithm is not converging")
+                    return -1,-1,self.structure
+
+            if track:
+                # download np.ndarray in structure_process directory by name
+                np.save(f"structure_process/{name}.npy",track)
+            progress_time=time.time()-st
+            return progress_time*1000,cnt,self.structure
         else:
-            return self.structure
+            print("Target is unreachable")
+            return -1,-1,self.structure
 
-    def plot(self)->None:
-        fig=plt.figure()
-        ax=fig.add_subplot(111,projection='3d')
-        for i in range(self.structure.shape[0]-1):
-            ax.plot([self.structure[i,0],self.structure[i+1,0]],[self.structure[i,1],self.structure[i+1,1]],[self.structure[i,2],self.structure[i+1,2]],c='r')
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
+    def solve1(self,target:np.ndarray,tolerance:float,a:float,b:float,track:bool=False,name:str='wow')->(float,int,np.ndarray):
+        if track:
+            track=np.ndarray()
+            np.concatenate((track,self.structure),axis=0)
+        if self.isreachable(target):
+            self.target=target.copy()
+            cnt=0
+            st=time.time()
+            while (diff:=dist(self.structure[-1],self.target))>tolerance:
+                self.__forward1(a,b)
+                self.__backward()
+                cnt+=1
+                if track:
+                    np.concatenate((track,self.structure),axis=0)
+                if cnt>200:
+                    print("This Algorithm is not converging")
+                    return -1,-1,self.structure
+            if track:
+                np.save(f"structure_process/{name}.npy",track)
+            progress_time=time.time()-st
+            return progress_time*1000,cnt,self.structure
+        else:
+            print("Target is unreachable")
+            return -1,-1,self.structure
 
-        plt.show()
+    # def plot(self)->None:
+    #     fig=plt.figure()
+    #     ax=fig.add_subplot(111,projection='3d')
+    #     for i in range(self.structure.shape[0]-1):
+    #         ax.plot([self.structure[i,0],self.structure[i+1,0]],[self.structure[i,1],self.structure[i+1,1]],[self.structure[i,2],self.structure[i+1,2]],c='r')
+    #     ax.set_xlabel('X')
+    #     ax.set_ylabel('Y')
+    #     ax.set_zlabel('Z')
+    #
+    #     plt.show()
--- test.py
+++ test.py
@@ -1,7 +1,217 @@
+from pandas import DataFrame
+
 import fabrik
 import numpy as np
-a=np.array(((0,0,0),(3,4,0),(7,7,0),(10,7,4),(10,7,8)))
-b=np.array((5,5,5,4))
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-model=fabrik.Structure(a,b)
-model.plot()
+class Tester:
+    def iteration_cost(self,model:fabrik.Structure,target:np.ndarray,tolerance:float=0.05,track:bool=False,name:str='wow')->float:
+        f=fabrik.Fabrik()
+        f.fit_model(model)
+        progress_time,cnt,structure=f.solve(target,tolerance,track,name)
+        return cnt if cnt!=-1 else np.nan
+
+    def iteration_cost1(self,model:fabrik.Structure,target:np.ndarray,tolerance:float=0.05,a:float=0.5,b:float=0.5,track:bool=False,name1:str='wow')->float:
+        f=fabrik.Fabrik()
+        f.fit_model(model)
+        progress_time1,cnt1,structure1=f.solve1(target,tolerance,a,b,track,name1)
+        return cnt1 if cnt1!=-1 else np.nan
+
+    def iteration_cost_average(self,model_list:list,target_list:list,tolerance:float=0.05)->float:
+        total=0
+        for i in range(len(model_list)):
+            tmp=self.iteration_cost(model_list[i],target_list[i],tolerance)
+            if tmp == np.nan:
+                return np.nan
+            total+=tmp
+
+        return total/len(model_list)
+
+    def iteration_cost_average1(self,model_list:list,target_list:list,tolerance:float=0.05,a:float=0.5,b:float=0.5)->float:
+        total1=0
+        for i in range(len(model_list)):
+            tmp1=self.iteration_cost1(model_list[i],target_list[i],tolerance,a,b)
+            if tmp1==np.nan:
+                return np.nan
+            total1+=tmp1
+
+        return total1/len(model_list)
+
+    def motion_cost(self,model:fabrik.Structure,target:np.ndarray,tolerance:float=0.05,mode:bool=True)->float:
+        f=fabrik.Fabrik()
+        f.fit_model(model)
+        progress_time,cnt,structure=f.solve(target,tolerance)
+        if cnt==-1:
+            return np.nan
+        motion_cost=0
+        for i in range(1,structure.shape[0]):
+            if mode:
+                motion_cost+=np.linalg.norm(model.structure[i]-structure[i])
+            else:
+                motion_cost+=np.linalg.norm(model.structure[i]-structure[i])*(model.length[i:].sum()/model.length.sum())
+        return motion_cost
+
+    def motion_cost1(self,model:fabrik.Structure,target:np.ndarray,tolerance:float=0.05,a:float=0.5,b:float=0.5,mode:bool=True)->float:
+        f=fabrik.Fabrik()
+        f.fit_model(model)
+        progress_time1,cnt1,structure1=f.solve1(target,tolerance,a,b)
+        print(structure1)
+        if cnt1==-1:
+            return np.nan
+        motion_cost1=0
+        for i in range(1,structure1.shape[0]):
+            if mode:
+                motion_cost1+=np.linalg.norm(model.structure[i]-structure1[i])
+            else:
+                motion_cost1+=np.linalg.norm(model.structure[i]-structure1[i])*(model.length[i:].sum()/model.length.sum())
+        return motion_cost1
+
+
+    def motion_cost_average(self,model_list:list,target_list:list,tolerance:float=0.05,mode:bool=True)->float:
+        total=0
+        for i in range(len(model_list)):
+            tmp=self.motion_cost(model_list[i],target_list[i],tolerance,mode)
+            total+=tmp
+            if tmp==np.nan:
+                return np.nan
+
+        return total/len(model_list)
+
+    def motion_cost_average1(self,model_list:list,target_list:list,tolerance:float=0.05,a:float=0.5,b:float=0.5,mode:bool=True)->float:
+        total1=0
+        for i in range(len(model_list)):
+            tmp1=self.motion_cost1(model_list[i],target_list[i],tolerance,a,b,mode)
+            total1+=tmp1
+            if tmp1==np.nan:
+                return np.nan
+
+        return total1/len(model_list)
+
+    def test(self,model_list:list,target_list:list,tolerance:float=0.05,mode:bool=True)-> DataFrame:
+        result=[]
+        motion_cost=self.motion_cost_average(model_list,target_list,tolerance,mode)
+        iteration_cost=self.iteration_cost_average(model_list,target_list,tolerance)
+        result.append([iteration_cost,motion_cost])
+
+        return pd.DataFrame(result,columns=['iteration_cost','motion_cost'])
+
+    def test1(self,model_list:list,target_list:list,tolerance:float=0.05,final_a:float=2,final_b:float=2,step_a:float=0.05,step_b:float=0.05,mode:bool=True)-> DataFrame:
+        result1=[]
+        total_iteration =int(((final_a // step_a + 1) * (final_b // step_b + 1))  )
+        present_iteration=0
+        for a in np.arange(-2,final_a,step_a):
+            for b in np.arange(-2,final_b,step_b):
+                motion_cost1=self.motion_cost_average1(model_list,target_list,tolerance,a,b,mode)
+                iteration_cost1=self.iteration_cost_average1(model_list,target_list,tolerance,a,b)
+                result1.append([iteration_cost1,motion_cost1,a,b])
+                present_iteration+=1
+                print(f"iteration:{present_iteration}/{total_iteration}")
+
+        return pd.DataFrame(result1,columns=['iteration_cost','motion_cost','a','b'])
+
+    def optimize1(self,model_list:list,target_list:list,tolerance:float=0.05,final_a:float=2,final_b:float=2,step_a:float=0.05,step_b:float=0.05,mode:bool=True)-> DataFrame:
+        loss1_df=self.test1(model_list,target_list,tolerance,final_a,final_b,step_a,step_b,mode)
+        # find minimum loss1 in loss1_df by a,b
+        min_loss1_df=loss1_df.sort_values(by='iteration_cost')
+
+        return min_loss1_df
+
+    def optimize1_with_result(self,result1_df:DataFrame)->DataFrame:
+        return result1_df.sort_values(by='iteration_cost')
+
+    def plot(self,model_list:list,target_list:list,tolerance:float=0.05,final_a:float=2,final_b:float=2,step_w:float=0.05,step_a:float=0.05,step_b:float=0.05,mode:bool=True)->None:
+        min_loss1_df=self.optimize1(model_list,target_list,tolerance,final_a,final_b,step_w,step_a,step_b,mode)
+        loss_df=self.test(model_list,target_list,tolerance,step_w,mode)
+
+        # plot loss_df and min_loss1_df by w_time
+        self.plot_with_result(loss_df,min_loss1_df)
+
+
+    def plot_with_result(self,result_df:DataFrame,result1_df:DataFrame)->None:
+        # scatter plot loss_df of loss by w_time and plot min_loss1_df of loss1 by w_time in the same plot
+        plt.figure(figsize=(10,6))
+        sns.scatterplot(data=result_df,x='w_iteration',y='loss',label='loss')
+        sns.scatterplot(data=result1_df,x='w_iteration',y='loss1',label='loss1')
+        plt.show()
+
+
+
+
+"""
+ex) Test Case
+
+np.ndarray, np.ndarray
+각각 structure, length
+structure 의 shape은 (n,3) 이고 length의 shape은 (n-1,) 이다.
+
+"""
+class TestCase:
+    def example(self):
+        self.structure = np.array([[0., 0., 0.], [1., 0., 0.], [2., 0., 0.], [3., 0., 0.], [4., 0., 0.]])
+        self.length = np.array([1., 1., 1., 1.])
+        self.target = np.array([3., 1., 0.])
+        return [fabrik.Structure(self.structure, self.length)], [self.target]
+
+    def build_structure_straight(self, n: int = 5, t: int = 50) -> None:
+        list_structure = []
+        list_target = []
+        for j in range(t):
+            self.structure = [np.array([0, 0, 0])]
+            for i in range(1, n):
+                length = 1.0
+                self.structure.append(self.structure[-1] + np.array([length, 0, 0]))
+            self.structure = np.array(self.structure)
+            self.length = np.array([np.linalg.norm(self.structure[i + 1] - self.structure[i]) for i in range(n - 1)])
+            self.target = self.structure[0] + np.random.uniform(-1.5, 1.5, 3)
+            list_structure.append(fabrik.Structure(self.structure, self.length))
+            list_target.append(self.target)
+        return list_structure, list_target
+
+    def build_structure_straight_random(self, n: int = 5, t: int = 50) -> None:
+        list_structure = []
+        list_target = []
+        for j in range(t):
+            self.structure = [np.array([0, 0, 0])]
+            for i in range(1, n):
+                length = np.random.uniform(0.5, 1.5)
+                self.structure.append(self.structure[-1] + np.array([length, 0, 0]))
+            self.structure = np.array(self.structure)
+            self.length = np.array([np.linalg.norm(self.structure[i+1] - self.structure[i]) for i in range(n-1)])
+            self.target = self.structure[0] + np.random.uniform(-1.5, 1.5, 3)
+            list_structure.append(fabrik.Structure(self.structure, self.length))
+            list_target.append(self.target)
+        return list_structure, list_target
+
+    def build_structure_angles(self, n: int = 5, t: int = 50) -> None:
+        list_structure = []
+        list_target = []
+        for j in range(t):
+            self.structure = [np.array([0, 0, 0])]
+            length = 1.0
+            for i in range(1, n):
+                angle = np.random.uniform(0, 2 * np.pi)
+                self.structure.append(self.structure[-1] + length * np.array([np.cos(angle), np.sin(angle), 0]))
+            self.structure = np.array(self.structure)
+            self.length = np.array([length for _ in range(n-1)])
+            self.target = self.structure[0] + np.random.uniform(-1, 1, 3)
+            list_structure.append(fabrik.Structure(self.structure, self.length))
+            list_target.append(self.target)
+        return list_structure, list_target
+
+    def build_structure_random(self, n: int = 5, t: int = 50) -> None:
+        list_structure = []
+        list_target = []
+        for j in range(t):
+            self.structure = [np.array([0, 0, 0])]
+            for i in range(1, n):
+                length = np.random.uniform(0.5, 1.5)
+                angle = np.random.uniform(0, 2 * np.pi)
+                self.structure.append(self.structure[-1] + length * np.array([np.cos(angle), np.sin(angle), 0]))
+            self.structure = np.array(self.structure)
+            self.length = np.array([np.linalg.norm(self.structure[i+1] - self.structure[i]) for i in range(n-1)])
+            self.target = self.structure[0] + np.random.uniform(-1.5, 1.5, 3)
+            list_structure.append(fabrik.Structure(self.structure, self.length))
+            list_target.append(self.target)
+        return list_structure, list_target
